d5de3edef5a5545fdd163c52ef6871f26338d936
Leo Cassarani <leo.cassarani@me.com>
2012-05-09 22:38:52 +0100

    Refactor TextFormatter#render into smaller methods
    TextFormatter#calculate_column_sizes, you're next.

--- lib/tinytable/text_formatter.rb
+++ lib/tinytable/text_formatter.rb
@@ -13,25 +13,70 @@ class TinyTable
     def render
       @output.clear
       calculate_column_sizes
-
-      if @table.has_header?
-        hr
-        row(@table.header)
-      end
-
-      hr
-      @table.each_row { |r| row(r) }
-      hr
-
-      if @table.has_footer?
-        row(@table.footer)
-        hr
-      end
-
+      render_header
+      render_rows
+      render_footer
       @output
     end
 
   private
+
+    def render_header
+      if @table.has_header?
+        hr
+        render_row(@table.header)
+      end
+    end
+
+    def render_rows
+      hr
+      @table.each_row { |row| render_row(row) }
+      hr
+    end
+
+    def render_footer
+      if @table.has_footer?
+        render_row(@table.footer)
+        hr
+      end
+    end
+
+    def hr
+      append CORNER
+      @col_widths.each do |w|
+        append HORIZONTAL * (w + 2)
+        append CORNER
+      end
+      new_line
+    end
+
+    def render_row(row)
+      append VERTICAL
+      row.each_with_index { |cell, i| render_cell(cell, i) }
+      new_line
+    end
+
+    def render_cell(text, i)
+      append PADDING
+      append text
+
+      cell_width = @col_widths[i]
+      text_width = text.to_s.length
+      if text_width < cell_width
+        append PADDING * (cell_width - text_width)
+      end
+
+      append PADDING
+      append VERTICAL
+    end
+
+    def append(text)
+      @output << text.to_s
+    end
+
+    def new_line
+      append "\n"
+    end
 
     def calculate_column_sizes
       @col_widths = if @table.has_header?
@@ -61,43 +106,6 @@ class TinyTable
           end
         end
       end
-    end
-
-    def hr
-      append CORNER
-      @col_widths.each do |w|
-        append HORIZONTAL * (w + 2)
-        append CORNER
-      end
-      new_line
-    end
-
-    def row(r)
-      append VERTICAL
-      r.each_with_index { |c, i| cell(c, i) }
-      new_line
-    end
-
-    def cell(text, i)
-      append PADDING
-      append text
-
-      cell_width = @col_widths[i]
-      text_width = text.to_s.length
-      if text_width < cell_width
-        append PADDING * (cell_width - text_width)
-      end
-
-      append PADDING
-      append VERTICAL
-    end
-
-    def append(text)
-      @output << text.to_s
-    end
-
-    def new_line
-      append "\n"
     end
   end
 end
